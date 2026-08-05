8c6ae638ebb91f9cac1d3d012758b7a215b40f6f
Marukome0743 <jambalaya.pyoncafe@gmail.com>
2026-08-05 08:22:45 +0000

    chore: update biome settings

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   experimental: {
     inlineCss: true,
-    viewTransition: true,
   },
   output: "standalone",
   reactCompiler: true,
